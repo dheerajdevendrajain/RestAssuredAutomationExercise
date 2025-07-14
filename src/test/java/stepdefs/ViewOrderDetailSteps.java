7068d2c3d45e2df405a5066494c12e496c3558c0
dheerajdevendrajain <dheerajdevendrajain@gmail.com>
2025-07-14 22:59:35 +0530

    Delete order and get order details added in order.feature

---
+++ src/test/java/stepdefs/ViewOrderDetailSteps.java
@@ -0,0 +1,16 @@
+package stepdefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class ViewOrderDetails {
+    @And("I view order details for the all orders")
+    public void iViewOrderDetailsForTheAllOrders() {
+
+
+    }
+
+    @Then("the order details should match the expected product details")
+    public void theOrderDetailsShouldMatchTheExpectedProductDetails() {
+    }
+}
